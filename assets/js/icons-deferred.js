c131e3d9b7a660bbc6bab8f995330e76f60c6f53
Nathaniel Andreano <outiswebsolutions@gmail.com>
2025-02-06 17:28:18 -0500

    Update icons-deferred.js

--- assets/js/icons-deferred.js
+++ assets/js/icons-deferred.js
@@ -1,11 +1,11 @@
 // this file loads icons once in 10% visitors viewport
 const icons = document.querySelectorAll('.icon-lazy');
 const observer = new IntersectionObserver((entries, observer) => {
-  enteries.forEach(entry => {
+  entries.forEach(entry => {
     // Check if icon is in viewport
     if(entry.isIntersecting){
       // Make icon visible if in viewport
-      enteries.target.classList.add('icon-loaded');
+      entries.target.classList.add('icon-loaded');
       // Then stop monitoring the icons once loaded
       observer.unobserve(entry.target);  
         
